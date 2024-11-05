07fa6e349179580eef6a45868158335693b76baa
Neeti Singh <neetisingh82541@gmail.com>
2024-11-05 22:01:33 +0530

    checkout form

--- ecomsite/shop/views.py
+++ ecomsite/shop/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Products
+from .models import Order
 from django.core.paginator import Paginator
 # Create your views here.
 
@@ -24,4 +25,16 @@ def detail(request,id):
     return render(request,'shop/detail.html',{'product_object':product_object})
 
 def checkout(request):
+    if request.method == "POST":
+        items = request.POST.get('items',"")
+        name = request.POST.get('name',"")
+        email = request.POST.get('email',"")
+        address = request.POST.get("address","")
+        city = request.POST.get('city',"")
+        state = request.POST.get('state',"")
+        zipcode = request.POST.get('zipcode',"")
+
+        order = Order(items=items,name=name,email=email,address=address,city=city,state=state,zipcode=zipcode)
+        order.save() 
+
     return render(request,'shop/checkout.html')
