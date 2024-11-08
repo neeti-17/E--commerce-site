bb239c7f8aaab881f43391602758e563c244e0a8
Neeti Singh <neetisingh82541@gmail.com>
2024-11-08 23:59:29 +0530

    final

--- ecomsite/shop/views.py
+++ ecomsite/shop/views.py
@@ -18,23 +18,24 @@ def index(request) :
     product_objects = paginator.get_page(page)
     
     return render(request,'shop/index.html',{'product_objects':product_objects})
-
-
+ 
+ 
 def detail(request,id):
     product_object = Products.objects.get(id=id)
     return render(request,'shop/detail.html',{'product_object':product_object})
-
+    
 def checkout(request):
+ 
     if request.method == "POST":
-        items = request.POST.get('items',"")
+        items = request.POST.get('items','')
         name = request.POST.get('name',"")
         email = request.POST.get('email',"")
-        address = request.POST.get("address","")
+        address = request.POST.get('address',"")
         city = request.POST.get('city',"")
-        state = request.POST.get('state',"")
+        state =request.POST.get('state',"")
         zipcode = request.POST.get('zipcode',"")
-
-        order = Order(items=items,name=name,email=email,address=address,city=city,state=state,zipcode=zipcode)
-        order.save() 
-
+        total = request.POST.get('total',"")
+        order = Order(items=items,name=name,email=email,address=address,city=city,state=state,zipcode=zipcode,total=total)
+        order.save()
+ 
     return render(request,'shop/checkout.html')
